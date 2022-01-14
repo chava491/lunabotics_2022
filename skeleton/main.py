c4c2afddd63c672d053f5f6ce36f5dd1346febfc
blackhawks28 <49330074+blackhawks28@users.noreply.github.com>
2022-01-14 14:27:30 -0600

    main files completed
    This commit includes the main.py, main.launch, and main.wrapper.py files. The wrapper file has been edited to accommodate this years robot in the GUI. Some there are a few GUI design features that will remain in the code for the time being until the robot can be tested with the GUI changes.

---
+++ skeleton/main.py
@@ -0,0 +1,15 @@
+from locomotion import Locomotion
+from digging import Digging
+from dumping import Dumping
+
+class Robot:
+
+    def __init__(self):
+        self.state = "manual"
+
+    def get_state(self):
+        return state
+    
+    def set_state(self, state):
+        self.state = state
+
