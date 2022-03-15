9ac7ef2417bdce6c77194fe9e3a2accdd2d6b28a
acarrasco0908 <acarrasco0908@gmail.com>
2022-03-15 01:46:36 -0500

    Added stepper motor testing script

---
+++ digging/stepper_motor_test_1.py
@@ -0,0 +1,20 @@
+import subprocess
+from yaml import load, dump
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+ 
+def ticcmd(*args):
+  return subprocess.check_output(['ticcmd'] + list(args))
+ 
+status = load(ticcmd('-s', '--full'), Loader=Loader)
+ 
+position = status['Current position']
+print("Current position is {}.".format(position))
+ 
+new_target = -200 if position > 0 else 200
+print("Setting target position to {}.".format(new_target))
+ticcmd('--exit-safe-start', '--position', str(new_target))
+ticcmd('--resume')
