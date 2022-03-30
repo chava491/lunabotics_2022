c6e3fb189fc25a33bc82f91407623b45699eebd6
ibo789 <ibo789@yahoo.com>
2022-03-30 15:55:23 -0500

    added arduino sketch for testing laser receiver module

---
+++ Arduino/Laser_receiver_test/Laser_receiver_test.ino
@@ -0,0 +1,32 @@
+/* This script was written to test the performance of the 
+ * cheap chinese laser reciever modules that we purchased 
+ * from Amazon
+ * 
+ * Written by: Abraham Yakisan
+ * Date: 3/30/2022
+ */
+
+//pins
+const int laser_read_pin = 2;
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("===============================");
+  Serial.println("Starting Laser Reciever Test...");
+  Serial.println("===============================");
+
+  pinMode(laser_read_pin, INPUT);
+}
+
+void loop() {
+  bool hit = digitalRead(laser_read_pin);  //bool value
+  String msg1 = "HIT OCURRED AT: ";
+  String msg2 = " ms";
+  if(hit){
+    String hit_msg = msg1 + millis() + msg2;
+    Serial.println(hit_msg); 
+  }
+
+  //delay(1);
+
+}
