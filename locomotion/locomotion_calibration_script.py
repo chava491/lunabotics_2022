39e9cc5dce44446b2e59f29b0ec6ffaadee78664
blackhawks28 <49330074+blackhawks28@users.noreply.github.com>
2022-05-02 19:16:03 -0500

    Big Odrive Changes
    Description of troubleshooting process for locomotion odrive calibration. As well as a new folder strictly for odrive files.

--- locomotion/locomotion_calibration_script.py
+++ locomotion/locomotion_calibration_script.py
@@ -3,12 +3,69 @@
 """
 Script that is used to perform setup and calibration of locomotion motors
 
+There are a few situations that need to be taken into account when running this script, and any errors that might occur
+while calibrating.
+
+This is documentation for the main errors that have been run into while setting up the o-drive for the locomotion motors.
+If other errors occur that are not discussed in this documentation, look up the error number by typing "odrv0.axis0.motor"
+when calibrating the motor and the error number when calibrating the encoders by typing "odrv0.axis0.encoder", as well
+as "dump_errors(odrv0)" to check error titles after every calibration test. Then you will have to do independent research
+of those errors in relation to your situation. The official o-drive error and parameter documentation, which can be found
+at https://docs.odriverobotics.com/v/latest/fibre_types/com_odriverobotics_ODrive.html , can be helpful in certain
+situations.
+
+1. The locomotion o-drive NEEDS to be powered by a battery and not from a power supply. The power supplies do NOT supply
+   enough current for the motors and o-drive, while the battery does supply enough current.
+        No matter what, if you try to calibrate the locomotion o-drive and it is powered by a power supply that does NOT
+        supply enough current, you will get past the motor calibration step, but will not get past the hall encoder
+        calibration step. Again, you will pass "odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION", but you
+        will NOT pass "odrv0.axis0.requested_state = AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION". You will run into
+        a system error that says "DC_BUS_UNDER_VOLTAGE". Even though if you look under "odrv0.axis0.encoder" it will say
+        "error: 0" it says that because the calibration process never even completed. The power supply's current
+        spiked and the voltage dropped, causing the power supply to protect itself and turn off and on again during the
+        hall encoder calibration step.
+
+2. When running this calibration script for the locomotion motors, the motors can NOT be under any sort of load at all.
+   So, if the motors are on the robot, the chains need to be removed from the single gear attached to the sprocket on both
+   motors.
+        That also goes for the Auger motor when running it on the Auger o-drive, there can NOT be any load on the motor.
+
+3. If you run the first calibration step "odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION", check
+   "odrv0.axis0.motor" and see if "error: 0". Whether it is 0 or not, ALWAYS look at the errors by typing
+   "dump_errors(odrv0).
+        If the error is "PHASE_RESISTANCE_OUT_OF_RANGE" then you need to change the ".motor.config.calibration_current"
+        lower. Default is 10, good practice would be to set it to 2.
+
+        If the error is "CURRENT_LIMIT_VIOLATION" then the value of ".motor.config.current_lim" +
+        ".motor.config.current_lim_margin" is less than the current that is being supplied to the o-drive, therefore it will
+        stop itself from having too much current supplied to it. Setting both ".motor.config.current_lim" and
+        ".motor.config.current_lim_margin" equal to 8, is a good starting point. We know that the motors WILL operate
+        under those conditions.
+
+        If the error is "BRAKE_RESISTOR_DISARMED" make sure that "odrv0.config.enable_brake_resistor" is set to "True".
+        Then check the value of "odrv0.config.brake_resistance" and make sure that it is set to 2. The locomotion o-drive
+        must use one of the 2 ohm brake resistors. If either of those parameters are changed, you NEED to run
+        "odrv0.save_configuration()" right after changing those parameters.
+            If the error "BRAKE_RESISTOR_DISARMED" occurs any other time, it is usually associated with another error.
+
+4. When running each of the different calibrations, you NEED to pass each one before moving onto the next. You CANNOT do
+   the "odrv0.axis0.requested_state = AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION" without passing
+   "odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION". Same with the 2nd and 3rd calibration tests,
+   you CANNOT run "odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION" before running
+   "odrv0.axis0.requested_state = AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION". Each calibration relies on the previous
+   calibration. So if there is an error after running "odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION"
+   you need to solve it before moving on. If there is an error after running
+   "odrv0.axis0.requested_state = AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION" you need to solve it before moving on.
+   If there is an error after running "odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION", you need to
+   solve it in order for the calibration process to be completed and the motors to properly function.
+
+
 @created: Mar. 18, 2022
 """
 
 import odrive
 from odrive.enums import *
-import fibre.libfibre
+import fibre
 import time
 import math
 import py_compile
@@ -21,7 +78,7 @@ if __name__ == '__main__':
     print("and run calibration sequences")
     print("=========================================================================")
     print("Searching for odrive, this may take a few seconds...")
-    odrv0 = odrive.find_any()
+    odrv0 = odrive.find_any()#serial_number="207939834D4D"
     print("odrive found...")
 
     #Setup parameters for motors
